79dd6c5ce2e9d09d5be0fd162f1b876f6fef629f
Christine Dodrill <christine.dodrill@lightspeedhq.com>
2020-06-22 09:30:21 -0400

    shell.nix: support linux

--- shell.nix
+++ shell.nix
@@ -2,20 +2,21 @@ let
   sources = import ./nix/sources.nix;
   mozilla = import sources.nixpkgs-mozilla;
   pkgs = import sources.nixpkgs { overlays = [ mozilla ]; };
-  darwinDeps = [  ];
+  darwinDeps = if pkgs.stdenv.isDarwin then
+    [ pkgs.darwin.apple_sdk.frameworks.Security ]
+  else
+    [ ];
 in pkgs.mkShell {
-  buildInputs = with pkgs; [
-    # rust
-    latest.rustChannels.stable.rust
-    cargo-watch
+  buildInputs = with pkgs;
+    [
+      # rust
+      latest.rustChannels.stable.rust
+      cargo-watch
 
-    # dependencies
-    libgit2
-    libiconv
-    openssl
-    pkg-config
-
-    # darwin
-    darwin.apple_sdk.frameworks.Security
-  ];
+      # dependencies
+      libgit2
+      libiconv
+      openssl
+      pkg-config
+    ] ++ darwinDeps;
 }
